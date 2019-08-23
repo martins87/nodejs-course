95b86fb1f61e7c5f9051d296a218ce9dd257e9d9
Daniel Martins <martinsda@gmail.com>
2019-08-23 16:53:35 -0300

    Edit 3 weather api request

---
+++ weather-app/utils/geocode.js
@@ -0,0 +1,24 @@
+const request = require('request')
+const chalk = require('chalk')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibWFydGluczg3IiwiYSI6ImNqem85djl0dTA1ZXozcG5vd2RxZ3JvMmsifQ.gtFEy9QK1ra8h3adKe42HA&limit=1'
+    
+    request({url: url, json: true}, (err, res) => {
+        if(err) {
+            callback('Unable to connect to mapbox weather service', undefined)
+        } else if(res.body.message) {
+            callback(chalk.red.inverse('\nMapbox') + ' Error: Wrong URL', undefined)
+        } else if(res.body.features.length === 0) {
+            callback(chalk.red.inverse('\nMapbox') + ' Error: No matching results', undefined)
+        } else {
+            callback(undefined, {
+                location: res.body.features[0].place_name,
+                latitude: res.body.features[0].center[1],
+                longitude: res.body.features[0].center[0]
+            })
+        }
+    })
+}
+
+module.exports = geocode
--- weather-app/weather-api-request.js
+++ weather-app/weather-api-request.js
@@ -1,26 +1,43 @@
 const request = require('request')
+const chalk = require('chalk')
 
 // Weather API 1: darksky
 const darkskyUrl ='https://api.darksky.net/forecast/97e191801c1744f5fa5a8d4ae6270703/-19.917299,-43.934559?units=si&lang=pt'
 
 request({url: darkskyUrl, json: true}, (err, res) => {
-    var temperature = res.body.currently.temperature
-    var precipProbability = res.body.currently.precipProbability
-    console.log(res.body.daily.data[0].summary + ' Temperatura agora: ' + temperature + ' oC. Há uma chance de ' + precipProbability + '% de chover hoje.')
+    if(err) {
+        console.log('There was an error trying to connect with the darksky weather service')
+    } else if(res.body.error) {
+        console.log(chalk.red.inverse('Darksky') + ' Unable to find location')
+    } else {
+        var temperature = res.body.currently.temperature
+        var precipProbability = res.body.currently.precipProbability
+        console.log(chalk.green.inverse('Darksky'))
+        console.log(res.body.daily.data[0].summary + ' Temperatura agora: ' + temperature + ' oC. Há uma chance de ' + precipProbability + '% de chover hoje.')
+    }
 })
 
 // Weather API 2: mapbox
 // mapbox token = pk.eyJ1IjoibWFydGluczg3IiwiYSI6ImNqem85djl0dTA1ZXozcG5vd2RxZ3JvMmsifQ.gtFEy9QK1ra8h3adKe42HA
-const mapboxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/belo&horizonte.json?access_token=pk.eyJ1IjoibWFydGluczg3IiwiYSI6ImNqem85djl0dTA1ZXozcG5vd2RxZ3JvMmsifQ.gtFEy9QK1ra8h3adKe42HA&limit=1'
+const mapboxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/berlin.json?access_token=pk.eyJ1IjoibWFydGluczg3IiwiYSI6ImNqem85djl0dTA1ZXozcG5vd2RxZ3JvMmsifQ.gtFEy9QK1ra8h3adKe42HA&limit=1'
 
 request({url: mapboxUrl, json: true}, (err, res) => {
-    var mostRelevantResult = res.body.features[0]
-
-    var cityName = mostRelevantResult.place_name
-    var latitude = mostRelevantResult.center[1]
-    var longitude = mostRelevantResult.center[0]
-
-    console.log(cityName)
-    console.log(latitude)
-    console.log(longitude)
+    if(err) {
+        console.log('There was an error trying to connect with the mapbox weather service')
+    } else if(res.body.message) {
+        console.log(chalk.red.inverse('Mapbox') + ' Error: Wrong URL')
+    } else if(res.body.features.length === 0) {
+        console.log(chalk.red.inverse('Mapbox') + ' Error: No matching results')
+    } else {
+        var mostRelevantResult = res.body.features[0]
+    
+        var cityName = mostRelevantResult.place_name
+        var latitude = mostRelevantResult.center[1]
+        var longitude = mostRelevantResult.center[0]
+    
+        console.log(chalk.green.inverse('Mapbox'))
+        console.log(cityName)
+        console.log(latitude)
+        console.log(longitude)
+    }
 })
