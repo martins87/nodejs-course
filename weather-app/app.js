747273ebc8dbddc4496171e16beec1e25790105d
Daniel Martins <martinsda@gmail.com>
2019-08-29 15:16:43 -0300

    Add web application

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,26 +1,74 @@
+const express = require('express')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+const path = require('path')
+const bodyParser = require('body-parser')
 
-if(process.argv.length === 3) {
-    var location = process.argv[2]
-} else {
-    return console.error('No location provided')
-}
+const app = express()
 
-// Belo Horizonte: -19.917299, -43.934559
-geocode(location, (err, data) => {
-    if(err) {
-        return console.log(err)
-    }
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-    // callback chaining
-    forecast(data.latitude, data.longitude, (err, forecastData) => {
-        if(err) {
-            console.log(err)
-        }
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
-        console.log(data.location)
-        console.log(forecastData)
+app.get('/', (req, res) => {
+    res.render('index', {
+        forecastData: '...'
     })
 })
 
+app.post('/', (req, res) => {
+    var location = req.body._location
+
+    geocode(location, (err, data) => {
+        if(err) {
+            return console.log(err)
+        }
+    
+        // callback chaining
+        forecast(data.latitude, data.longitude, (err, forecastData) => {
+            if(err) {
+                console.log(err)
+            }
+
+            console.log(data.location)
+            console.log(forecastData.summary)
+
+            var fullData = '' + data.location + ' [' + data.latitude + ', ' + data.longitude + '] : ' + forecastData.summary
+
+            res.render('index', {
+                forecastData: fullData
+            })
+        })
+    })
+
+    // res.render('index', {
+    //     forecastData: forecastData
+    // })
+
+})
+
+// Belo Horizonte: -19.917299, -43.934559
+// geocode('Berlin', (err, data) => {
+//     if(err) {
+//         return console.log(err)
+//     }
+
+//     // callback chaining
+//     forecast(data.latitude, data.longitude, (err, forecastData) => {
+//         if(err) {
+//             console.log(err)
+//         }
+
+//         console.log(data.location)
+//         console.log(forecastData)
+//     })
+// })
+
+const port = 3000
+app.listen(port, () => {
+    console.log('Server listening on port ' + port)
+})
--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -2,21 +2,21 @@ const request = require('request')
 const chalk = require('chalk')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + longitude + ',' + latitude + '.json?access_token=pk.eyJ1IjoibWFydGluczg3IiwiYSI6ImNqem85djl0dTA1ZXozcG5vd2RxZ3JvMmsifQ.gtFEy9QK1ra8h3adKe42HA'
+    // const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + longitude + ',' + latitude + '.json?access_token=pk.eyJ1IjoibWFydGluczg3IiwiYSI6ImNqem85djl0dTA1ZXozcG5vd2RxZ3JvMmsifQ.gtFEy9QK1ra8h3adKe42HA'
 
-    request({url: url, json: true}, (err, res) => {
-        if(err) {
-            callback('Unable to connect to mapbox weather service', undefined)
-        } else if(res.body.message) {
-            callback(chalk.red.inverse('Mapbox') + ' Error: Wrong URL', undefined)
-        } else if(res.body.features.length === 0) {
-            callback(chalk.red.inverse('Mapbox') + ' Error: No matching results', undefined)
-        } else {
-            callback(undefined, {
-                location: res.body.features[0].place_name
-            })
-        }
-    })
+    // request({url: url, json: true}, (err, res) => {
+    //     if(err) {
+    //         callback('Unable to connect to mapbox weather service', undefined)
+    //     } else if(res.body.message) {
+    //         callback(chalk.red.inverse('Mapbox') + ' Error: Wrong URL', undefined)
+    //     } else if(res.body.features.length === 0) {
+    //         callback(chalk.red.inverse('Mapbox') + ' Error: No matching results', undefined)
+    //     } else {
+    //         callback(undefined, {
+    //             location: res.body.features[0].place_name
+    //         })
+    //     }
+    // })
 
     const url2 = 'https://api.darksky.net/forecast/97e191801c1744f5fa5a8d4ae6270703/' + longitude + ',' + latitude + ''
 
@@ -27,7 +27,7 @@ const forecast = (latitude, longitude, callback) => {
             callback(chalk.red.inverse('Darksky') + ' Unable to find location', undefined)
         } else {
             callback(undefined, {
-                location: res.body.daily.data[0].summary
+                summary: res.body.daily.data[0].summary
             })
         }
     })
