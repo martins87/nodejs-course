9db6e8ba71005361f25fbcbd59877c7353677571
Daniel Martins <martinsda@gmail.com>
2019-09-11 11:46:22 -0300

    Add fetching api

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,17 +1,22 @@
 console.log('Loaded here')
 
-fetch('https://puzzle.mead.io/puzzle').then((res) => {
-    res.json().then((data) => {
-        console.log(data)
-    })
-})
+// fetch('https://puzzle.mead.io/puzzle').then((res) => {
+//     res.json().then((data) => {
+//         console.log(data)
+//     })
+// })
 
-const address = 'Berlin'
-
-const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibWFydGluczg3IiwiYSI6ImNqem85djl0dTA1ZXozcG5vd2RxZ3JvMmsifQ.gtFEy9QK1ra8h3adKe42HA'
+const city = 'Berlin'
+const url = 'http://localhost:3030/weather?address=' + city
 
 fetch(url).then((res) => {
     res.json().then((data) => {
-        console.log(data)
+        if(data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.address)
+            console.log(data.location)
+            console.log(data.forecast)
+        }
     })
 })
