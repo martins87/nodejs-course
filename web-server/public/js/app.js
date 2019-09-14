81f36a2234a7bfd5c3d05be5a50ced2b7964fb3d
Daniel Martins <martinsda@gmail.com>
2019-09-14 12:18:31 -0300

    Edit form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,20 +1,3 @@
-console.log('Loaded here')
-
-const city = 'Berlin'
-const url = 'http://localhost:3030/weather?address=' + city
-
-fetch(url).then((res) => {
-    res.json().then((data) => {
-        if(data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.address)
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
-})
-
 // document.querySelector() returns the whole form
 const weatherForm = document.querySelector('form')
 const searchData = document.querySelector('input')
@@ -36,7 +19,19 @@ weatherForm.addEventListener('submit', (e) => {
 
     const location = searchData.value
 
-    console.log('inputData: ', location)
+    const url = 'http://localhost:3030/weather?address=' + location
+
+    fetch(url).then((res) => {
+        res.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.address)
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
+    })
 
 })
 
