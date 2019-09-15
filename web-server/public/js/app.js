396937e02dd721a153ab9645bbbd3f60b1c255e0
Daniel Martins <martinsda@gmail.com>
2019-09-15 15:03:47 -0300

    Edit css

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,6 +2,10 @@
 const weatherForm = document.querySelector('form')
 const searchData = document.querySelector('input')
 
+// # gets the id of the element
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
 /** event listeners:
  *  - there are many:
  *      for hovering something (e.g. the header)
@@ -17,21 +21,28 @@ weatherForm.addEventListener('submit', (e) => {
     // which is reload the page every time the form is submitted
     e.preventDefault()
 
-    const location = searchData.value
+    messageOne.textContent = ''
+    messageTwo.textContent = 'Loading...'
 
+    const location = searchData.value
     const url = 'http://localhost:3030/weather?address=' + location
 
     fetch(url).then((res) => {
         res.json().then((data) => {
-            if (data.error) {
-                console.log(data.error)
+
+            // console.log(data)
+
+            if(data.err) {
+                messageTwo.textContent = 'Unable to find location. Try another search.'
+            } else if(data.error) {
+                messageTwo.textContent = data.error
             } else {
-                console.log(data.address)
-                console.log(data.location)
-                console.log(data.forecast)
+                console.log(data.location + ': ' + data.forecast)
+
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast.summary
             }
         })
     })
 
 })
-
