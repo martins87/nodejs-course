98b78334e618dbe04c0354de43f6719d35fc2f2e
Daniel Martins <martinsda@gmail.com>
2019-09-18 11:17:07 -0300

    Edit forecast temperatures

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -5,8 +5,9 @@ const chalk = require('chalk')
  * add new data to forecast (e.g. temperature high and low for the day)
  */
 
+//  password: name123
 const forecast = (latitude, longitude, callback) => {
-    const url2 = 'https://api.darksky.net/forecast/97e191801c1744f5fa5a8d4ae6270703/' + longitude + ',' + latitude + ''
+    const url2 = 'https://api.darksky.net/forecast/97e191801c1744f5fa5a8d4ae6270703/' + latitude + ',' + longitude + ''
 
     request({url: url2, json: true}, (err, { body }) => {
         if(err) {
@@ -15,15 +16,19 @@ const forecast = (latitude, longitude, callback) => {
             callback(chalk.red.inverse('Darksky') + ' Unable to find location', undefined)
         } else {
             var forecastData = body.daily.data[0].summary
-            forecastData += ' It is currently ' + body.currently.temperature + ' degrees out. '
-            forecastData += 'The high today is ' + body.daily.data[0].temperatureHigh + ' degrees '
-            forecastData += 'and the low is ' + body.daily.data[0].temperatureLow + ' degrees. '
+            forecastData += ' It is currently ' + body.currently.temperature + '°F (' + getCelsiusTemperature(body.currently.temperature) + '°C) out. '
+            forecastData += 'The high today is ' + body.daily.data[0].temperatureHigh + '°F (' + getCelsiusTemperature(body.daily.data[0].temperatureHigh) + '°C) '
+            forecastData += 'and the low is ' + body.daily.data[0].temperatureLow + '°F (' + getCelsiusTemperature(body.daily.data[0].temperatureLow) + '°C).'
             forecastData += 'There is a ' + body.currently.precipProbability + '% chance of rain.'
             callback(undefined, {
                 summary: forecastData
             })
         }
     })
+}
+
+function getCelsiusTemperature(fahrenheit) {
+    return ((fahrenheit - 32) * 5/9).toFixed(2)
 }
 
 module.exports = forecast
