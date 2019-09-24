4364116c29a7402a14b7cb9c6d9f0d8ff621321d
Daniel Martins <martinsda@gmail.com>
2019-09-24 10:32:54 -0300

    Add ObjectID

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,8 +1,15 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionUrl = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+// generate new id
+const id = new ObjectID()
+console.log('id: ', id)
+console.log('timestamp: ', id.getTimestamp())
 
 // http://mongodb.github.io/node-mongodb-native/3.2/api/
 MongoClient.connect(connectionUrl, {
@@ -18,33 +25,55 @@ MongoClient.connect(connectionUrl, {
     // reference to the database
     const db = client.db(databaseName)
 
-    // db.collection('users').insertOne({
-    //     name: 'Léa',
-    //     age: 24 
-    // }, (err, result) => {
+    db.collection('users').insertOne({
+        _id: new ObjectID(),
+        name: 'Daniella',
+        age: 30
+    }, (err, result) => {
+        if(err) {
+            console.log('Unable to insert user')
+        }
+
+        console.log(result.ops)
+    })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Edmundo',
+    //         age: 66
+    //     },
+    //     {
+    //         name: 'Rosalicy',
+    //         age: 53
+    //     }
+    // ], (err, result) => {
     //     if(err) {
-    //         console.log('Unable to insert user')
+    //         return console.log('Unable to insert documents')
     //     }
 
     //     console.log(result.ops)
     // })
 
-    db.collection('users').insertMany([
+    var tasks = [
         {
-            name: 'Edmundo',
-            age: 66
-        },
-        {
-            name: 'Rosalicy',
-            age: 53
+            description: 'Node.js course',
+            completed: false
+        }, {
+            description: 'Angular course',
+            completed: false
+        }, {
+            description: 'Java Spring Boot course',
+            completed: false
         }
-    ], (err, result) => {
-        if(err) {
-            return console.log('Unable to insert documents')
-        }
+    ]
 
-        console.log(result.ops)
-    })
+    // db.collection('tasks').insertMany(tasks, (err, result) => {
+    //     if(err) {
+    //         return console.log('Unable to insert documents')
+    //     }
+
+    //     console.log(result.ops)
+    // })
 })
 
 // console.log('%c My friends', 'color: orange; font-weight: bold')
